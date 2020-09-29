540eb5bc418ef02f18d832ee33916f22b9a8e25a
Noura Loudani <contact.nouraloudani@gmail.com>
2020-09-29 17:07:34 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,5 @@
 Def display_board(board)
-   puts #{board[0]} + , #{board[1]}, |, #{board[2]} 
+   puts "#{board[0]}"+ , #{board[1]}, |, #{board[2]} 
         "-----------"
         " #{board[3]} | #{board[4]} | #{board[5]} "
         "-----------"
